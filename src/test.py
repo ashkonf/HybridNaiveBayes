3479bf429cbebdab5729fcdc5af5340819d9d1dc
Ashkon Farhangi <ashkon.f@gmail.com>
2018-02-25 16:23:58 -0800

    test.py style improvements

--- src/test.py
+++ src/test.py
@@ -2,20 +2,26 @@ import requests
 import nb
 import distributions
 
-# This data set is described here: https://archive.ics.uci.edu/ml/datasets/Statlog+(German+Credit+Data)
-raw_data = requests.get("https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data").text.strip()
+def load_data():
+    # Loading, formatting and partitioning the data set:
+    
+    # This data set is described here: https://archive.ics.uci.edu/ml/datasets/Statlog+(German+Credit+Data)
+    raw_data = requests.get("https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data").text.strip()
 
-lines = raw_data.split("\n")
-value_matrix = [line.split() for line in lines]
-data_points = [values[:-1] for values in value_matrix]
-labels = [values[-1] for values in value_matrix]
+    lines = raw_data.split("\n")
+    value_matrix = [line.split() for line in lines]
+    data_points = [values[:-1] for values in value_matrix]
+    labels = [values[-1] for values in value_matrix]
 
-data_set_slice = len(data_points) / 2
-training_set = (data_points[:data_set_slice], labels[:data_set_slice])
-test_set = (data_points[data_set_slice:], labels[data_set_slice:])
+    data_set_slice = len(data_points) / 2
+    training_set = (data_points[:data_set_slice], labels[:data_set_slice])
+    test_set = (data_points[data_set_slice:], labels[data_set_slice:])
+
+    return (training_set, test_set)
 
 def featurizer(data_point):
-    # Massaging a data point into the format the Naive Bayes classifier implementation expects of feature vectors:
+    # Massaging a data point into the format the NB classifier implementation expects of feature vectors:
+    
     return [
         # Bucketed and therefore categorical:
         nb.Feature("Checking account status", distributions.Multinomial, data_point[0]),
@@ -80,6 +86,7 @@ def featurizer(data_point):
 
 def main():
     # Creating, training and testing the classifier:
+    training_set, test_set = load_data()
     classifier = nb.NaiveBayesClassifier(featurizer)
     classifier.train(training_set[0], training_set[1])
     print "Accuracy = %s" % classifier.accuracy(test_set[0], test_set[1])
